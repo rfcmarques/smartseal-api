4e4c1a53c66f249b8a186c827d6945c05c8e9939
Rui Marques <a.marques1967@gmail.com>
2024-11-22 19:44:45 +0000

    feat: configure MongoDB connection using environment variables and update connection options

--- config/db.js
+++ config/db.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const user = process.env.MONGO_USER;
+const password = process.env.MONGO_PASSWORD;
+const uri = `mongodb+srv://${user}:${password}@smartseal-cluster.k4h9f.mongodb.net/?retryWrites=true&w=majority&appName=smartseal-cluster`;
+
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
 
 const db = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(uri, clientOptions);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err);
